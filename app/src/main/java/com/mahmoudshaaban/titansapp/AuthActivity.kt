ccd2b090537fc7efa167bb73336422e3ea61767b
LENOVO <cumody82@gmail.com>
2020-09-15 22:30:20 +0200

    Initial commit

---
+++ app/src/main/java/com/mahmoudshaaban/titansapp/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.mahmoudshaaban.titansapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+
+    }
+}
