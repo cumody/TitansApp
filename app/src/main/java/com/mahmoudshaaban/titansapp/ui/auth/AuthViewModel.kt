2ab73853ae2b1e85a4c77604fba5cd804e6a02b3
LENOVO <cumody82@gmail.com>
2020-09-16 02:00:13 +0200

    Arch of the App

---
+++ app/src/main/java/com/mahmoudshaaban/titansapp/ui/auth/AuthViewModel.kt
@@ -0,0 +1,11 @@
+package com.mahmoudshaaban.titansapp.ui.auth
+
+import androidx.lifecycle.ViewModel
+import com.mahmoudshaaban.titansapp.repository.auth.AuthRepository
+
+class AuthViewModel(val authRepository: AuthRepository) : ViewModel() {
+
+
+
+
+}
