e0af93dda02fee0b032f51d9c485c95e2d29b80a
LENOVO <cumody82@gmail.com>
2020-09-23 04:30:14 +0200

    CustomBottomNavController Handler Class

--- app/src/main/java/com/mahmoudshaaban/titansapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/mahmoudshaaban/titansapp/ui/main/MainActivity.kt
@@ -3,24 +3,63 @@ package com.mahmoudshaaban.titansapp.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mahmoudshaaban.titansapp.R
 import com.mahmoudshaaban.titansapp.ui.BaseActivity
 import com.mahmoudshaaban.titansapp.ui.auth.AuthActivity
+import com.mahmoudshaaban.titansapp.ui.main.account.ChangePasswordFragment
+import com.mahmoudshaaban.titansapp.ui.main.account.UpdateAccountFragment
+import com.mahmoudshaaban.titansapp.ui.main.blog.UpdateBlogFragment
+import com.mahmoudshaaban.titansapp.ui.main.blog.ViewBlogFragment
+import com.mahmoudshaaban.titansapp.util.BottomNavController
+import com.mahmoudshaaban.titansapp.util.BottomNavController.*
+import com.mahmoudshaaban.titansapp.util.setUpNavigation
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(),
+    NavGraphProvider,
+    OnNavigationGraphChanged,
+    OnNavigationReselectedListener {
+
+
+    private lateinit var bottomNavigationView : BottomNavigationView
+
+    private val bottomNavController by lazy (LazyThreadSafetyMode.NONE ){
+        BottomNavController(
+            this,
+            R.id.main_fragments_container,
+            R.id.nav_blog ,
+            this ,
+            this
+        )
+
+    }
+
+
     override fun displayProgressBar(bool: Boolean) {
-        if (bool){
+        if (bool) {
             progress_bar.visibility == View.VISIBLE
         } else {
             progress_bar.visibility = View.INVISIBLE
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar()
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.setUpNavigation(bottomNavController,this)
+        if (savedInstanceState == null){
+            bottomNavController.onNavigationItemSelected()
+        }
+
 
         tool_bar.setOnClickListener {
             sessionManager.logout()
@@ -29,19 +68,77 @@ class MainActivity : BaseActivity() {
         subscribeObservers()
     }
 
-    fun subscribeObservers(){
-        sessionManager.cachedToken.observe(this, Observer{ authToken ->
+    private fun setSupportActionBar(){
+        setSupportActionBar(tool_bar)
+    }
+
+
+    override fun onBackPressed() =  bottomNavController.onBackPressed()
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            android.R.id.home -> onBackPressed()
+        }
+
+
+        return super.onOptionsItemSelected(item)
+
+    }
+
+    fun subscribeObservers() {
+        sessionManager.cachedToken.observe(this, Observer { authToken ->
             Log.d(TAG, "MainActivity, subscribeObservers: ViewState: ${authToken}")
-            if(authToken == null || authToken.account_pk == -1 || authToken.token == null){
+            if (authToken == null || authToken.account_pk == -1 || authToken.token == null) {
                 navAuthActivity()
                 finish()
             }
         })
     }
 
-    private fun navAuthActivity(){
+    private fun navAuthActivity() {
         val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun getNavGraphId(itemId: Int) = when(itemId) {
+        R.id.nav_blog -> {
+            R.navigation.nav_blog
+        }
+        R.id.nav_account -> {
+            R.navigation.nav_account
+        }
+        R.id.nav_create_blog -> {
+            R.navigation.nav_create_blog
+        } else -> {
+            R.navigation.nav_blog
+
+        }
+
+    }
+
+    override fun onGraphChange() {
+        // TODO what needs to happen when the graph changes ?
+
+    }
+
+    override fun onReselectNavItem(navController: NavController, fragment: Fragment) = when(fragment){
+
+        is ViewBlogFragment -> {
+            navController.navigate(R.id.action_viewBlogFragment_to_blogFragment)
+        }
+        is UpdateBlogFragment -> {
+            navController.navigate(R.id.action_updateBlogFragment_to_blogFragment)
+        }
+        is UpdateAccountFragment -> {
+            navController.navigate(R.id.action_updateAccountFragment_to_accountFragment)
+        }
+        is ChangePasswordFragment -> {
+            navController.navigate(R.id.action_changePasswordFragment_to_accountFragment)
+        }
+        else -> {
+            // DO NOTHING
+        }
+
     }
 }
--- app/src/main/java/com/mahmoudshaaban/titansapp/ui/main/blog/ViewBlogFragment.kt
+++ app/src/main/java/com/mahmoudshaaban/titansapp/ui/main/blog/ViewBlogFragment.kt
@@ -1,9 +1,8 @@
 package com.mahmoudshaaban.titansapp.ui.main.blog
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.navigation.fragment.findNavController
 import com.mahmoudshaaban.titansapp.R
 
 class ViewBlogFragment : BaseBlogFragment(){
@@ -19,5 +18,38 @@ class ViewBlogFragment : BaseBlogFragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        // TODO check if the user is author of blog post
+        val isAuhorOfBlogPost = true
+        if (isAuhorOfBlogPost){
+            inflater.inflate(R.menu.edit_view_menu,menu)
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val isAuhorOfBlogPost = true
+        if (isAuhorOfBlogPost){
+
+            when(item?.itemId){
+                R.id.edit -> {
+                    navUpdateBlogFragment()
+                    return true
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
+
+    }
+
+    fun navUpdateBlogFragment(){
+        findNavController().navigate(R.id.action_viewBlogFragment_to_updateBlogFragment)
     }
 }
